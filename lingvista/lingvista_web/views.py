19cee154a40a3fa573bd95a6f4d1fd9bbc3c883e
Stepan <stepankuleshov13@gmail.com>
2025-02-28 16:41:01 +0100

    added button logic, register and login page

--- lingvista/lingvista_web/views.py
+++ lingvista/lingvista_web/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'html/pages/index.html')
 
+def login_view(request):
+    return render(request, 'html/pages/login_page.html')
+
+def register_view(request):
+    return render(request, 'html/pages/registry_page.html')
+
+def tasks_view(request):
+    return render(request, 'html/pages/tasks_page.html')
