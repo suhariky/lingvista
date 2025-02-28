09438b1db6b00ec1b16f95e626f9746fbbb9da0a
Stepan <stepankuleshov13@gmail.com>
2025-02-28 17:19:23 +0100

    added button logic

--- lingvista/lingvista/urls.py
+++ lingvista/lingvista/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from lingvista_web.views import index, login_view, register_view, tasks_view
+from lingvista_web.views import index, login_view, register_view, tasks_view, profile_view, leaderboard_view, dictionary_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('tasks/', tasks_view, name='tasks'),
+    path('profile/', profile_view, name='profile'),
+    path('leaderboard/', leaderboard_view, name='leaderboard'),
+    path('dictionary/', dictionary_view, name='dictionary'),
 ]
--- lingvista/lingvista_web/views.py
+++ lingvista/lingvista_web/views.py
@@ -11,3 +11,12 @@ def register_view(request):
 
 def tasks_view(request):
     return render(request, 'html/pages/tasks_page.html')
+
+def profile_view(request):
+    return render(request, 'html/pages/account_page.html')
+
+def leaderboard_view(request):
+    return render(request, 'html/pages/leaderboard_page.html')
+
+def dictionary_view(request):
+    return render(request, 'html/pages/dictionary_page.html')
