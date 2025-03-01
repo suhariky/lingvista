998bf1cb27084530209fdc119ed86b74677969dd
Stepan <stepankuleshov13@gmail.com>
2025-03-01 11:01:17 +0100

    added final version of button logic at main_page

--- lingvista/lingvista/urls.py
+++ lingvista/lingvista/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from lingvista_web.views import index, login_view, register_view, tasks_view, profile_view, leaderboard_view, dictionary_view
+from lingvista_web.views import index, login_view, register_view, langlevel_view, profile_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
-    path('tasks/', tasks_view, name='tasks'),
-    path('profile/', profile_view, name='profile'),
-    path('leaderboard/', leaderboard_view, name='leaderboard'),
-    path('dictionary/', dictionary_view, name='dictionary'),
+    path('login_page/', login_view, name='login'),
+    path('registry_page/', register_view, name='register'),
+    path('langlevel_page/', langlevel_view, name='langlevel'),
+    path('account_page/', profile_view, name='profile'),
 ]
