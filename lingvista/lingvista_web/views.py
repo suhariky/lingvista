2ebc17f9655567c197367dfd224071b77dbb658a
qwerty <you@example.com>
2025-03-02 19:00:40 +0300

    Add Mark results (authentication, registry and logging pages)

--- lingvista/lingvista_web/views.py
+++ lingvista/lingvista_web/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm, ProfileEditForm
+from .models import Profile
+
 
 def main_page(request):
     return render(request, 'html/pages/main_page.html')
@@ -7,7 +13,18 @@ def login_view(request):
     return render(request, 'html/pages/login_page.html')
 
 def register_view(request):
-    return render(request, 'html/pages/registry_page.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(user=user)  # Создаем профиль
+            login(request, user)
+            messages.success(request, 'Регистрация прошла успешно.')
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'html/pages/registry_page.html', {'form': form})
 
 def tasks_view(request):
     return render(request, 'html/pages/tasks_page.html')
@@ -26,4 +43,25 @@ def lessons_view(request, level):
         'level': level.upper(),
     }
     return render(request, 'html/pages/lessons_page.html', context)
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html', {'user': request.user})
+
+@login_required
+def edit_profile(request):
+    profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = ProfileEditForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Профиль обновлён.')
+            return redirect('profile')
+    else:
+        form = ProfileEditForm(instance=profile)
+    return render(request, 'user/profile_edit.html', {'form': form})
+
+@login_required
+def profile_history(request):
+    return render(request, 'user/profile_history.html')
 
